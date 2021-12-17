0916b95e2da0798de37c5ce6dfb3640006f6e9d8
pclin87 <73913675+pclin87@users.noreply.github.com>
2021-12-17 11:01:23 -1000

    10

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -54,7 +54,20 @@ function MagicProduct(id, name, price, expiryDate, points, isBonus){
 // Establish inheritance between Product() & MagicProduct() here
 MagicProduct.prototype = Object.create(Product.prototype)
 // Define Rating class here
-
+class Rating {
+    constructor (){
+        this.rate = ""
+        }
+        set rating(value){
+            if(value > 1 && value <= 4 ){
+                return rate = "OK"
+            } else if (value >= 5 && value <= 7){
+                return rate ="GOOD"
+            }else if (value > 7){
+                rate = "EXCEPTIONAL"
+            } else {rate = "BAD"}
+    }
+}
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
     let a = new Array();
