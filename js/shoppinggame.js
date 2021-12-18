f5886b84e8bf337b3832fa41354e3174f68ea811
pclin87 <73913675+pclin87@users.noreply.github.com>
2021-12-18 08:46:57 -1000

    6-1

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -280,6 +280,7 @@ function init(data) {
 
         rl.question("What's your name? ", function (name) {
             // Assign the player object's name property to the user entered name here
+            name = player.name
             console.log(`Welcome ${player.name} !!!`.blue);
             start(data);
         });
