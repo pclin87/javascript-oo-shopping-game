13ca2fa071bb4bac69fa92b555f9534d6ba89e87
pclin87 <73913675+pclin87@users.noreply.github.com>
2021-12-17 15:06:39 -1000

    4-1

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -208,7 +208,13 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function(){
+        if (id === Product.id) {
+            return true
+        } else { return false}
+     } 
+};
 
 // Complete this function
 const generateProductId = () => {};
