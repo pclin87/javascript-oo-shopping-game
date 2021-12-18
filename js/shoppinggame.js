cc8624de1ce66dec4fce4e0884d3a7b322c88080
pclin87 <73913675+pclin87@users.noreply.github.com>
2021-12-18 09:14:12 -1000

    7-1

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -347,6 +347,7 @@ function init(data) {
     // Complete this function
     const rateAndExit = () => {
         // Create a new instance of Rating and assign it to a variable named playerRating here
+        playerRating = new Rating 
         rl.question("How would you rate this game on a scale of 1-10 (1 being the lowest)?:", function (r) {
             if (r == "" || isNaN(r) || r == 0 || r > 10) {
                 console.log("Invalid rating! Please nter a number from 1 - 10".red);
